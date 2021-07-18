6ab457b3155c0bc846e0be7ced55d296d5adf33e
erica-joseph <ericagjoseph@outlook.com>
2021-07-18 13:31:49 -0400

    Initial commit

---
+++ src/main/java/ucf/assignment/Main.java
@@ -0,0 +1,37 @@
+package ucf.assignment;
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Erica Joseph
+ */
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import java.util.Objects;
+
+public class Main extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/ucf/assignment/MainInterface.fxml"))); //access the file needed to load FXML; this will be the source of the code
+            Scene scene = new Scene(root); // attach scene graph to scene
+            stage.setTitle("Main"); // displayed in window's title bar
+            //stage.getIcons().add(new Image("/resources/ucf/assignment/Style/ObentoLogoo4.png"));
+            stage.setScene(scene); // attach scene to stage
+            scene.getStylesheets().add("ucf/assignment/Style/Inventory.css"); //stylesheet on hand
+            stage.show(); // display the stage
+        }
+        catch (Exception ignored){
+
+        }
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    } //man call to launch
+}
